50d25dd6df1a2b5d60c3ed12c038844d62886a1c
Student <Student@LabVM2047644>
2026-04-13 15:36:43 +0200

    Add project files.

---
+++ CyberApp_part1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CyberApp_part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chatbot z = new Chatbot();
+            z.Start();
+        }
+    }
+}
